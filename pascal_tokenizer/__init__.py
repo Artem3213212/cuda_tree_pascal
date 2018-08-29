98693b36eeb5cde6a92041e9588d280730ce86f6
Artem3213212 <cx-1212@yandex.ru>
2018-08-29 13:27:41 +0300

    Fix #30

--- pascal_tokenizer/__init__.py
+++ pascal_tokenizer/__init__.py
@@ -232,5 +232,8 @@ class PasTokenizerParallelStack(PasTokenizerStack):
 
     def stop(self):
         self.main.ended = True
-        while not self.queue.get_nowait()[3]:
+        try:
+            while not self.queue.get_nowait()[3]:
+                pass
+        except:
             pass
